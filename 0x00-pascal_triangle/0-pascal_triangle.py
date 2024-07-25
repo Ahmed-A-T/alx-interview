5b92f5111b34224703ee8410085915fad6bba7a7
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-07-26 02:04:31 +0300

    Fxing Bugs

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,24 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
 def pascal_triangle(n):
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
+    k = []
     if n <= 0:
-        return []
-
-    triangle = [[1]]  # Initialize the first row of the triangle
-
+        return k
+    k = [[1]]
     for i in range(1, n):
-        row = [1]  # Every row starts with a 1
-        for j in range(1, i):
-            # Each element is the sum of the two elements above it
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)  # Every row ends with a 1
-        triangle.append(row)
-
-    return triangle
-
-# Example usage:
-n = 5
-result = pascal_triangle(n)
-for row in result:
-    print(row)
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        temp.append(1)
+        k.append(temp)
+    return k
